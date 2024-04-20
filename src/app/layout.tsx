91b119fbfa54cfcb1e1689ab2335f94e1f3280fe
Michael Nahkies <michael@nahkies.co.nz>
2024-04-20 19:05:59 +0100

    fix: oops

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type {Metadata} from "next"
-import {Inter} from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({subsets: ["latin"]})
 import theme from "@/theme"
 import {ThemeProvider} from "@mui/material"
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter"
@@ -20,7 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
       <head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
