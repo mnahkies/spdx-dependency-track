851de2faa9b2283923f1fff4b0379d5752688f2f
Michael Nahkies <michael@nahkies.co.nz>
2024-04-20 18:18:24 +0100

    fix: work around hacky use of koa runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+  output: "standalone",
+  webpack: (
+    config,
+    {buildId, dev, isServer, defaultLoaders, nextRuntime, webpack}
+  ) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // TODO: HACK: the alpha typescript-nextjs template is using the
+      //       koa runtime which doesn't play nicely with nextjs
+      koa: false,
+      '@koa/cors': false,
+      'koa-body': false,
+    }
+    // Important: return the modified config
+    return config
+  },
 };
 
 export default nextConfig;
